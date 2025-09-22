f1fa93934a76e200cec2a6631d2bb79913d500d2
abhishekbansal123726 <abhishekbansal2806@gmail.com>
2025-09-23 00:57:35 +0530

    Update que02.c

--- Experiment1/que02.c
+++ Experiment1/que02.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Printing address in multiple lines
+    printf("Abhishek Bansal\n");
+    printf("123, MG Road\n");
+    printf("Dehradun, Uttarakhand\n");
+    printf("India\n");
+    return 0;
+}
 
