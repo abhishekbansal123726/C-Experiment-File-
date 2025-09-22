d74de23cd6ab4acd1eada8369393fa65b34b2ad3
abhishekbansal123726 <abhishekbansal2806@gmail.com>
2025-09-23 01:04:38 +0530

    Update exp3.2_que02.c

--- Experiment3/exp3.2_que02.c
+++ Experiment3/exp3.2_que02.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int num, i;
+
+    // Input number
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("\nMultiplication table of %d:\n", num);
+
+    // Loop to print table
+    for (i = 1; i <= 10; i++) {
+        printf("%d * %d = %d\n", num, i, num * i);
+    }
+
+    return 0;
+}
+
 
