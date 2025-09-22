0c950a24e285452521442c5a6ac817082570ccac
abhishekbansal123726 <abhishekbansal2806@gmail.com>
2025-09-23 00:59:38 +0530

    Update que02.c

--- Experiment2/que02.c
+++ Experiment2/que02.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    float celsius, fahrenheit;
+
+    // Input temperature in Celsius
+    printf("Enter temperature in Celsius: ");
+    scanf("%f", &celsius);
+
+    // Conversion formula
+    fahrenheit = (celsius * 9 / 5) + 32;
+
+    // Output result
+    printf("Temperature in Fahrenheit = %.2f\n", fahrenheit);
+
+    return 0;
+}
 
