666c15e82d14a16f8e3ae3132c913ad2fb464762
abhishekbansal123726 <abhishekbansal2806@gmail.com>
2025-09-23 00:59:09 +0530

    Update que01.c

--- Experiment2/que01.c
+++ Experiment2/que01.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    float length, width, area, perimeter;
+
+    // Input length and width
+    printf("Enter the length of the rectangle: ");
+    scanf("%f", &length);
+
+    printf("Enter the width of the rectangle: ");
+    scanf("%f", &width);
+
+    // Calculations
+    area = length * width;
+    perimeter = 2 * (length + width);
+
+    // Output
+    printf("\nArea of rectangle = %.2f\n", area);
+    printf("Perimeter of rectangle = %.2f\n", perimeter);
+
+    return 0;
+}
 
