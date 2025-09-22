23adc3168384e24375155f22c7151532ce48559b
abhishekbansal123726 <abhishekbansal2806@gmail.com>
2025-09-23 01:05:51 +0530

    Update exp3.2_que04.c

--- Experiment3/exp3.2_que04.c
+++ Experiment3/exp3.2_que04.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    double population = 100000;
+    double rate = 10; // 10%
+    int year;
+
+    printf("Year\tPopulation\n");
+
+    for (year = 1; year <= 10; year++) {
+        population = population * (1 + rate / 100);
+        printf("%d\t%.0lf\n", year, population);
+    }
+
+    return 0;
+}
 
